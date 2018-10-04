943dc3a44d25011a4d7b84f0f39d548ee58872d8
BrianLeBlanc98 <brianleblanc98@hotmail.com>
2018-10-04 19:11:06 -0400

    Added box that can move left and right

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	Transform parentTransform;
+
+	// Use this for initialization
+	void Start () {
+		parentTransform = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.D)) {
+			parentTransform.position = parentTransform.position + new Vector3 (1, 0);
+		}else if (Input.GetKey(KeyCode.A)) {
+			parentTransform.position = parentTransform.position + new Vector3 (-1, 0);
+		}
+	}
+}
