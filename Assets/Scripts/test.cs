b8cd8a1b97d389ee0eb9d2a41e74e406fdfcf638
Aaron Ramos-Lazette <ramosa_aa@hotmail.com>
2018-10-23 09:00:13 -0400

    Basic physics added to box, camera parented to player

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -4,19 +4,54 @@ using UnityEngine;
 
 public class test : MonoBehaviour {
 
-	Transform parentTransform;
+	//private Transform parentTransform;
+
+    private const float speed = 1.0f;
+    private const float fallSpeed = 1.0f;
+    private Rigidbody2D rb;
+    public Transform pCam;
+    private bool falling;
 
 	// Use this for initialization
 	void Start () {
-		parentTransform = gameObject.transform;
+        //rb = this.gameObject.GetComponent<Rigidbody2D>();
+        //parentTransform = gameObject.transform;
+        falling = true;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey(KeyCode.D)) {
-			parentTransform.position = parentTransform.position + new Vector3 (1, 0);
-		}else if (Input.GetKey(KeyCode.A)) {
-			parentTransform.position = parentTransform.position + new Vector3 (-1, 0);
-		}
-	}
+
+    void FixedUpdate()
+    {
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+        //float moveVertical = Input.GetAxis("Vertical");
+
+        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+        //rb.AddForce(movement * speed);
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = (falling) ? -fallSpeed : 0.0f;
+
+        this.gameObject.GetComponent<Transform>().position += new Vector3(moveHorizontal, fallSpeed, 0.0f);
+
+
+
+    }
+
+    private void Update()
+    {
+        falling = true;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.contacts.Length > 0)
+        {
+            ContactPoint contact = collision.contacts[0];
+            if (Vector3.Dot(contact.normal, Vector3.up) > 0.5)
+            {
+                falling = false;
+            }
+        }
+    }
+
 }
