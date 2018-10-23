014881250f38349bef6231dbc304b3c50f2d6ef4
BrianLeBlanc98 <brianleblanc98@hotmail.com>
2018-10-23 11:32:00 -0400

    Changed test.cs -> Player.cs

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour {
+
+    private Transform transform;
+    public float offset = 1.0f;
+
+	void Start () {
+        transform = this.gameObject.GetComponent<Transform>();
+        SetSide(Player.Orientation.RIGHT);
+	}
+
+	void Update () {
+
+	}
+
+    public void SwitchSide()
+    {
+        float currentX = transform.localPosition.x;
+        transform.localPosition = new Vector3(-currentX, 0.0f);
+    }
+
+    public void SetSide(Player.Orientation o)
+    {
+        transform.localPosition = new Vector3((int)o * offset, 0.0f);
+    }
+
+}
