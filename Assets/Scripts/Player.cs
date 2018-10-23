836cdd570b20e44d5b87c8e955fed12de3e49b68
BrianLeBlanc98 <brianleblanc98@hotmail.com>
2018-10-23 14:41:57 -0400

    Changed movement back to using rigidbodies and box colliders

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,44 +10,66 @@ public class Player : MonoBehaviour {
         LEFT = -1
     };
 
-    private const float speed = 1.0f;
-    private const float fallSpeed = 0.1f;
+    private const float speed = 4.0f;
+	private const float jumpSpeed = 8.0f;
+	private bool hasJumped = false;
+
+	private Vector3 moveDirection = Vector3.zero;
+
+	public Rigidbody rb;
     public Transform pCam;
     public Sword sword;
 
     Orientation orientation;
 
 	void Start () {
+		rb = GetComponent<Rigidbody> ();
         orientation = Orientation.RIGHT;
         sword.SetSide(Orientation.RIGHT);
 	}
 
-    private void Update()
-    {
+    private void FixedUpdate() {
+		Vector3 prev = moveDirection;
 		float moveHorizontal;
-		float input = Input.GetAxis ("Horizontal");
-		if (input > 0) {
-			moveHorizontal = speed;
-		}else if (input < 0) {
-			moveHorizontal = -speed;
-		}else{
-			moveHorizontal = 0;
+		float inputHorizontal = Input.GetAxis ("Horizontal");
+
+		if (inputHorizontal > 0) { moveHorizontal = speed; }
+		else if (inputHorizontal < 0) { moveHorizontal = -speed; }
+		else{ moveHorizontal = 0; }
+
+		if (moveHorizontal != 0) {
+			moveDirection.x = moveHorizontal;
+		} else {
+			moveDirection.x = 0;
 		}
-		CharacterController controller = GetComponent<CharacterController> ();
-		controller.SimpleMove (new Vector3(moveHorizontal, 0.0f, 0.0f));
 
-        if (moveHorizontal > 0)
-        {
-            if (orientation == Orientation.LEFT) sword.SwitchSide();
-            orientation = Orientation.RIGHT;
-        } 
-        else if (moveHorizontal < 0)
-        {
-            if (orientation == Orientation.RIGHT) sword.SwitchSide();
-            orientation = Orientation.LEFT;
-        }
-    }
+		if (Input.GetButton ("Jump") && !hasJumped) {
+			moveDirection.y = jumpSpeed;
+			hasJumped = true;
+		} else {
+			moveDirection.y = rb.velocity.y;
+		}
 
-	void OnControllerColliderHit(ControllerColliderHit hit){
+		// Sword Orientation
+		if (moveHorizontal > 0){
+			if (orientation == Orientation.LEFT) sword.SwitchSide();
+			orientation = Orientation.RIGHT;
+		} else if (moveHorizontal < 0){
+			if (orientation == Orientation.RIGHT) sword.SwitchSide();
+			orientation = Orientation.LEFT;
+		}
+			
+		rb.velocity = moveDirection;
+	}
+
+	void OnCollisionEnter(Collision collision){
+		// Check if collision happened at the bottom of the player
+		// Make it so they can jump again if they touch the floor and not walls
+		if (collision.contacts.Length > 0) {
+			ContactPoint contact = collision.contacts [0];
+			if (Vector3.Dot (contact.normal, Vector3.up) > 0.5) {
+				hasJumped = false;
+			}
+		}
 	}
 }
