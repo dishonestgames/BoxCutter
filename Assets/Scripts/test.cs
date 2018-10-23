53bcd188f4af91494c2ef46609dd849c168ae119
BrianLeBlanc98 <brianleblanc98@hotmail.com>
2018-10-23 10:28:27 -0400

    Changed player movement to use Character Controller instead

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -4,54 +4,21 @@ using UnityEngine;
 
 public class test : MonoBehaviour {
 
-	//private Transform parentTransform;
-
-    private const float speed = 1.0f;
-    private const float fallSpeed = 1.0f;
-    private Rigidbody2D rb;
+    private const float speed = 0.1f;
+    private const float fallSpeed = 0.1f;
     public Transform pCam;
-    private bool falling;
 
-	// Use this for initialization
 	void Start () {
-        //rb = this.gameObject.GetComponent<Rigidbody2D>();
-        //parentTransform = gameObject.transform;
-        falling = true;
+
 	}
-
-    void FixedUpdate()
-    {
-        //float moveHorizontal = Input.GetAxis("Horizontal");
-        //float moveVertical = Input.GetAxis("Vertical");
-
-        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
-
-        //rb.AddForce(movement * speed);
-
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = (falling) ? -fallSpeed : 0.0f;
-
-        this.gameObject.GetComponent<Transform>().position += new Vector3(moveHorizontal, fallSpeed, 0.0f);
-
-
-
-    }
 
     private void Update()
     {
-        falling = true;
+		CharacterController controller = GetComponent<CharacterController> ();
+		controller.SimpleMove (new Vector3(0.0f,0.0f,0.0f));
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.contacts.Length > 0)
-        {
-            ContactPoint contact = collision.contacts[0];
-            if (Vector3.Dot(contact.normal, Vector3.up) > 0.5)
-            {
-                falling = false;
-            }
-        }
-    }
-
+	void OnControllerColliderHit(ControllerColliderHit hit){
+		Debug.Log ("hit");
+	}
 }
