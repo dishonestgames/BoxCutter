44ce774049f0ed50d661b54c1d4a3473608a0ad7
BrianLeBlanc98 <brianleblanc98@hotmail.com>
2018-10-23 10:48:18 -0400

    Added left and right movement

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class test : MonoBehaviour {
 
-    private const float speed = 0.1f;
+    private const float speed = 1.0f;
     private const float fallSpeed = 0.1f;
     public Transform pCam;
 
@@ -14,11 +14,19 @@ public class test : MonoBehaviour {
 
     private void Update()
     {
+		float moveHorizontal;
+		float input = Input.GetAxis ("Horizontal");
+		if (input > 0) {
+			moveHorizontal = speed;
+		}else if (input < 0) {
+			moveHorizontal = -speed;
+		}else{
+			moveHorizontal = 0;
+		}
 		CharacterController controller = GetComponent<CharacterController> ();
-		controller.SimpleMove (new Vector3(0.0f,0.0f,0.0f));
+		controller.SimpleMove (new Vector3(moveHorizontal, 0.0f, 0.0f));
     }
 
 	void OnControllerColliderHit(ControllerColliderHit hit){
-		Debug.Log ("hit");
 	}
 }
